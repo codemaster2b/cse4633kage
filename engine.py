9b71ed40d2098d5e594619e5e1e3c5aa22f73104
Jon Butterfield <codemaster2b@yahoo.com>
2023-03-09 13:16:31 -0600

    process move actions

--- engine.py
+++ engine.py
@@ -1,13 +1,47 @@
 class Engine:
 
-    Board = []
-    BoardRows = 8
-    BoardColumns = 8
-
     def __init__(self):
-        self.Board = []
-        for i in range(self.BoardRows):
-            self.Board.append([])
-            for j in range(self.BoardColumns):
-                self.Board[i].append(1)
+        self.board = []
+        self.boardRows = 17
+        self.boardColumns = 17
+        for i in range(self.boardRows):
+            self.board.append([])
+            for j in range(self.boardColumns):
+                self.board[i].append(0)
+        
+        self.isPlayerOne = True
+        self.playerOneKing = [7, 7]
+        self.playerTwoKing = [9, 9]
+
+        self.board[6][7] = 1
+        self.board[8][7] = 1
+        self.board[7][6] = 1
+        self.board[7][8] = 1
+
+        self.board[self.playerOneKing[0]][self.playerOneKing[1]] = 1
+        self.board[self.playerTwoKing[0]][self.playerTwoKing[1]] = 2
+    
+    def validate(self, move):
+        if type(move) == type([]) and len(move) == 3 and type(move[1]) == type(1) and type(move[2]) == type(1):
+            return True
+        else:
+            return False
+    
+    def make_move(self, move):
+        if self.validate(move):
+            if move[0] == "K":
+                if self.isPlayerOne:
+                    self.board[self.playerOneKing[0]][self.playerOneKing[1]] = 0
+                    self.playerOneKing[0] = move[1]
+                    self.playerOneKing[1] = move[2]
+                    self.board[self.playerOneKing[0]][self.playerOneKing[1]] = 1
+                else:
+                    self.board[self.playerTwoKing[0]][self.playerTwoKing[1]] = 0
+                    self.playerTwoKing[0] = move[1]
+                    self.playerTwoKing[1] = move[2]
+                    self.board[self.playerTwoKing[0]][self.playerTwoKing[1]] = 2
+            elif move[0] == "W":
+                self.board[move[1]][move[2]] = 1
+
+            self.isPlayerOne = not self.isPlayerOne
 
--- start.py
+++ start.py
@@ -4,91 +4,15 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-import random
-import time
-
-class Pos(QWidget):
-    expandable = pyqtSignal(int, int)
-    clicked = pyqtSignal()
-    ohno = pyqtSignal()
-
-    def __init__(self, x, y, *args, **kwargs):
-        super(Pos, self).__init__(*args, **kwargs)
-
-        self.setFixedSize(QSize(20, 20))
-
-        self.x = x
-        self.y = y
-
-    def reset(self):
-        self.is_start = False
-        self.is_mine = False
-        self.adjacent_n = 0
-
-        self.is_revealed = False
-        self.is_flagged = False
-
-        self.update()
-
-    def paintEvent(self, event):
-        p = QPainter(self)
-        p.setRenderHint(QPainter.Antialiasing)
-
-        r = event.rect()
-
-        if self.is_revealed:
-            color = self.palette().color(QPalette.Background)
-            outer, inner = color, color
-        else:
-            outer, inner = Qt.gray, Qt.lightGray
-
-        p.fillRect(r, QBrush(inner))
-        pen = QPen(outer)
-        pen.setWidth(1)
-        p.setPen(pen)
-        p.drawRect(r)
-        p.drawPixmap(r, QPixmap(QImage("./images/bug.png")))
-        pen = QPen("blue")
-        p.setPen(pen)
-        f = p.font()
-        f.setBold(True)
-        p.setFont(f)
-        p.drawText(r, Qt.AlignHCenter | Qt.AlignVCenter, str(self.adjacent_n))
-
-    def flag(self):
-        self.is_flagged = True
-        self.update()
-
-        self.clicked.emit()
-
-    def reveal(self):
-        self.is_revealed = True
-        self.update()
-
-    def click(self):
-        if not self.is_revealed:
-            self.reveal()
-            if self.adjacent_n == 0:
-                self.expandable.emit(self.x, self.y)
-
-        self.clicked.emit()
-
-    def mouseReleaseEvent(self, e):
-
-        if (e.button() == Qt.RightButton and not self.is_revealed):
-            self.flag()
-
-        elif (e.button() == Qt.LeftButton):
-            self.click()
-
-            if self.is_mine:
-                self.ohno.emit()
-
+import math
+import engine
 
 class MainWindow(QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.engine = engine.Engine()
 
         self.playerOneBox = QGroupBox("Player 1")
         self.playerOneBox.width = 300
@@ -103,13 +27,13 @@ class MainWindow(QMainWindow):
         playerOneBoxLayout.addWidget(QLabel("AI"),0,0)
         playerOneBoxLayout.addWidget(playerOneAI,0,1)
 
-        playerOneMoveLabel = QLabel()
+        self.playerOneMoveLabel = QLabel()
         playerOneBoxLayout.addWidget(QLabel("MOVE"),1,0)
-        playerOneBoxLayout.addWidget(playerOneMoveLabel,1,1)
+        playerOneBoxLayout.addWidget(self.playerOneMoveLabel,1,1)
 
-        playerOneStatusLabel = QLabel()
+        self.playerOneStatusLabel = QLabel()
         playerOneBoxLayout.addWidget(QLabel("STATUS"),2,0)
-        playerOneBoxLayout.addWidget(playerOneStatusLabel,2,1)
+        playerOneBoxLayout.addWidget(self.playerOneStatusLabel,2,1)
 
         playerOneAutoCheckBox = QCheckBox("AUTO")
         playerOneBoxLayout.addWidget(playerOneAutoCheckBox,3,0)
@@ -129,13 +53,13 @@ class MainWindow(QMainWindow):
         playerTwoBoxLayout.addWidget(QLabel("AI"),0,0)
         playerTwoBoxLayout.addWidget(playerTwoAI,0,1)
 
-        playerTwoMoveLabel = QLabel()
+        self.playerTwoMoveLabel = QLabel()
         playerTwoBoxLayout.addWidget(QLabel("MOVE"),1,0)
-        playerTwoBoxLayout.addWidget(playerTwoMoveLabel,1,1)
+        playerTwoBoxLayout.addWidget(self.playerTwoMoveLabel,1,1)
 
-        playerTwoStatusLabel = QLabel()
+        self.playerTwoStatusLabel = QLabel()
         playerTwoBoxLayout.addWidget(QLabel("STATUS"),2,0)
-        playerTwoBoxLayout.addWidget(playerTwoStatusLabel,2,1)
+        playerTwoBoxLayout.addWidget(self.playerTwoStatusLabel,2,1)
 
         playerTwoAutoCheckBox = QCheckBox("AUTO")
         playerTwoBoxLayout.addWidget(playerTwoAutoCheckBox,3,0)
@@ -144,20 +68,13 @@ class MainWindow(QMainWindow):
 
         playerOneConfirmButton.clicked.connect(self.playerOneConfirmButton_clicked)
         playerTwoConfirmButton.clicked.connect(self.playerTwoConfirmButton_clicked)
-        self.set_player(True)
 
         self.boardLayout = QGridLayout()
         self.boardLayout.setSpacing(1)
 
-        movesText = QTextEdit()
-        movesText.width = 200
+        self.movesText = QTextEdit()
+        self.movesText.width = 200
 
-        cursor = QTextCursor(movesText.document())
-        cursor.setPosition(0)
-        movesText.setTextCursor(cursor)
-        movesText.insertPlainText('Your Move Mate')
-
-       
         forwardBackwardButtonLayout = QHBoxLayout()
         forwardBackwardButtonLayout.addSpacing(10)
         forwardBackwardButtonLayout.addWidget(QLabel("<<"),alignment=Qt.AlignCenter)
@@ -182,7 +99,7 @@ class MainWindow(QMainWindow):
         vLayout2 = QVBoxLayout()
         vLayout2.addSpacing(10)
         vLayout2.addWidget(QLabel("Moves Made"))
-        vLayout2.addWidget(movesText)
+        vLayout2.addWidget(self.movesText)
         vLayout2.addSpacing(40)
 
         hLayoutTop = QHBoxLayout()
@@ -206,65 +123,111 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(cw)
 
         self.init_map()
-        self.reset_map()
+        self.refresh()
         self.show()
 
+    def get_move(self, moveString):
+        text = moveString.replace("(","").replace(")","").replace("'","").replace(" ","")
+        split = text.split(",")
+        split[1] = int(split[1])
+        split[2] = int(split[2])
+        return split
+
+    def log_text(self, text):
+        self.movesText.append(text)
 
     def playerOneConfirmButton_clicked(self):
-        self.set_player(False)
-        return
+        if self.engine.isPlayerOne:
+            move = self.get_move(self.playerOneMoveLabel.text())
+            self.engine.make_move(move)
+            self.refresh()
 
     def playerTwoConfirmButton_clicked(self):
-        self.set_player(True)
-        return
+        if not self.engine.isPlayerOne:
+            move = self.get_move(self.playerTwoMoveLabel.text())
+            self.engine.make_move(move)
+            self.refresh()
 
-    def set_player(self, isPlayerOne):
-        if isPlayerOne:
+    def set_player_move(self, moveString):
+        move = self.get_move(moveString)
+        valid = self.engine.validate(move)
+        if self.engine.isPlayerOne:
+            self.playerOneMoveLabel.setText(moveString)
+            self.playerOneStatusLabel.setText(str(valid))
+        else:
+            self.playerTwoMoveLabel.setText(moveString)
+            self.playerTwoStatusLabel.setText(str(valid))
+
+    def init_map(self):
+        # Add positions to the map
+        for x in range(self.engine.boardColumns):
+            for y in range(self.engine.boardRows):
+                if x % 2 != 0 or y % 2 != 0:
+                    item_w = 30
+                    item_h = 30
+
+                    w = QPushButton("")
+                    w.text = str(x) + "," + str(y)
+                    pos = self.get_piece_at(w.text)
+                    if pos[0] == "W":
+                        if pos[1] % 2 == 0:
+                            item_h = 8
+                        if pos[2] % 2 == 0:
+                            item_w = 8
+
+                    w.setFixedSize(QSize(item_w, item_h))
+                    w.clicked.connect(self.board_clicked)
+                    self.boardLayout.addWidget(w, y, x)
+
+    def refresh(self):
+        if self.engine.isPlayerOne:
             self.playerOneBox.setStyleSheet("font-weight: bold")
             self.playerTwoBox.setStyleSheet("none")
         else:
             self.playerTwoBox.setStyleSheet("font-weight: bold")
             self.playerOneBox.setStyleSheet("none")
 
-    def init_map(self):
-        # Add positions to the map
-        for x in range(17):
-            for y in range(17):
-                w = QPushButton("")
-                w.setStyleSheet("background: white")
-                item_w = 30
-                if x % 2 == 0:
-                    item_w = 8
-
-                item_h = 30
-                if y % 2 == 0:
-                    item_h = 8
-
-                if x % 2 != 0 and y % 2 != 0:
-                    if x % 4 - y % 4 == 0:
-                        w.setStyleSheet("background: tan")
-                    else:
-                        w.setStyleSheet("background: brown")
-                else:
-                    w.setStyleSheet("background: black")
-
-
+        for x in range(self.engine.boardColumns):
+            for y in range(self.engine.boardRows):
                 if x % 2 != 0 or y % 2 != 0:
-                    w.setFixedSize(QSize(item_w, item_h))
-                    self.boardLayout.addWidget(w, y, x)
+                    w = self.boardLayout.itemAtPosition(y, x).widget()
+                    pos = self.get_piece_at(w.text)
+                    if pos[0] == "K":
+                        if pos[1] % 4 - pos[2] % 4 == 0:
+                            w.setStyleSheet("background: tan")
+                        else:
+                            w.setStyleSheet("background: brown")
 
-                # Connect signal to handle expansion.
-                #w.clicked.connect(self.trigger_start)
-                #w.expandable.connect(self.expand_reveal)
-                #w.ohno.connect(self.game_over)
+                        if self.engine.board[pos[1]][pos[2]] == 1:
+                            w.setText("K 1")
+                        elif self.engine.board[pos[1]][pos[2]] == 2:
+                            w.setText("K 2")
+                        else:
+                            w.setText("")
+                    elif pos[0] == "W":
+                        if self.engine.board[pos[1]][pos[2]] > 0:
+                            w.setStyleSheet("background: green")
+                        else:
+                            w.setStyleSheet("background: black")
 
-    def reset_map(self):
-        # Clear all mine positions
-        #for x in range(0, 8):
-            #for y in range(0, 8):
-                #w = self.boardLayout.itemAtPosition(y, x).widget()
-                #w.reset()
-        return
+
+    def board_clicked(self):
+        pos = self.get_piece_at(self.sender().text)
+        self.log_text(str(pos))
+        self.set_player_move(str(pos))
+
+    def get_piece_at(self, positionStr):
+        pos = positionStr.split(",")
+        y = int(pos[0])
+        x = int(pos[1])
+
+        piece = "K"
+        if x % 2 == 0 and y % 2 == 0:
+            piece = "No"
+        elif x % 2 == 0 or y % 2 == 0:
+            piece = "W"
+        
+        return piece, x, y
 
 if __name__ == '__main__':
     app = QApplication([])
